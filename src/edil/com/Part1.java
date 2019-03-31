0a30a344db64a30c4bd1ff5c37cb27b7d677e525
Yedil·Serzhan <39997211+edilserjan@users.noreply.github.com>
2019-03-31 20:38:38 +0600

    Part2

--- src/edil/com/Part1.java
+++ src/edil/com/Part1.java
@@ -7,7 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import edil.com.HuffmanCode;
 
 public class Part1 {
     private static String readFile(String path, Charset encoding) throws IOException {
@@ -20,10 +22,10 @@ public class Part1 {
     public static void main(String[] args) throws IOException {
 
         // map for char & int
-        Map<Character,Integer> charNums = new HashMap<Character, Integer>();
+        HashMap<Character,Integer> charNums = new HashMap<Character, Integer>();
 
         //map for char & double
-        Map<Character,Double> charDouble = new HashMap<Character, Double>();
+        HashMap<Character,Double> charDouble = new HashMap<Character, Double>();
 
 
         //open the input file, save as a string
@@ -46,24 +48,27 @@ public class Part1 {
             }
         }
 
-
-        DecimalFormat df = new DecimalFormat("#.####");
-        df.setRoundingMode(RoundingMode.CEILING);
+//
+//        DecimalFormat df = new DecimalFormat("#.####");
+//        df.setRoundingMode(RoundingMode.CEILING);
 
         for (Map.Entry<Character, Integer> entry : charNums.entrySet()) {
             char c = entry.getKey();
             double num = entry.getValue();
-            charDouble.put(c, Double.valueOf(df.format(num * 1.0/n)));
+            charDouble.put(c, num * 1.0/n);
+            System.out.println(c+": " + charDouble.get(c));
         }
 
 
-        for (Map.Entry<Character, Double> entry : charDouble.entrySet()) {
-            char c = entry.getKey();
-            double num = entry.getValue();
-            System.out.println(c+": " + num);
+        HuffmanCode huffmanCode = new HuffmanCode(s);
+
+        HashMap<Character,String> res = new HashMap<Character, String>();
+        res = huffmanCode.getCompressedResult();
+
+        for (Character c : res.keySet()) {
+            System.out.println(c+": "+ res.get(c));
         }
 
-//        System.out.println(charDouble);
 
     }
 }
